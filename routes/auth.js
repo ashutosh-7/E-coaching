3c1d575b543f5fa6f8cc9394e9b82874d36a6e6e
ashutosh-7 <accesssdenied777@gmail.com>
2020-06-30 01:06:02 +0530

    Final Authentication Update

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const express= require('express');
 const router= express.Router();
 const authController = require('../controllers/auth');
+const authSuccess= require('../middlewares/auth-success');
 
-
-router.get('/login',authController.getLogin);
-router.post('/login',authController.postLogin);
-router.get('/register',authController.getRegister);
-router.post('/register',authController.postRegister);
+router.get('/login',authSuccess,authController.getLogin);
+router.post('/login',authSuccess,authController.postLogin);
+router.get('/register',authSuccess,authController.getRegister);
+router.post('/register',authSuccess,authController.postRegister);
 router.post('/logout',authController.postLogout);
 
 
